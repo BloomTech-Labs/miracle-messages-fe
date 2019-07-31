f2d8f764862ca9c514990fde69be55b71ac927df
Hugo Martinez <manya01@protonmail.com>
2019-07-31 13:53:18 -0400

    added form reducer

---
+++ miracle-messages/reducers/FormReducer.js
@@ -0,0 +1,38 @@
+import {
+    ADDING_VOLUNTEER_START,
+    ADDING_VOLUNTEER_SUCCESS,
+    ADDING_VOLUNTEER_FAIL
+} from '../actions/FormActions';
+
+const initialState = {
+    isRegistered: false,
+    isFetching: false,
+    errors: null,
+    volunteer: {}
+};
+
+export const formReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case ADDING_VOLUNTEER_START:
+            return {
+                ...state,
+                isFetching: true,
+                isRegistered: false,
+                errors: null                
+            };
+        case ADDING_VOLUNTEER_SUCCESS:
+            return {
+                isFetching: false,
+                isRegistered: true,
+                errors: null,                
+            };
+        case ADDING_VOLUNTEER_FAIL: 
+            return {
+                isFetching: false,
+                isRegistered: false,
+                errors: action.payload
+            }
+        default:
+            return state;
+    }
+}
