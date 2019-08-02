34c40402b197916e46107b984c0820a6aa7370f8
Navid Karami <navid.gsz@gmail.com>
2019-08-02 11:02:34 -0700

    fixed the bug for adding a new volunteer

--- miracle-messages/src/actions/FormActions.js
+++ miracle-messages/src/actions/FormActions.js
@@ -1,61 +1,22 @@
 import axios from 'axios';
 
-export const ADD_VOLUNTEER_START='ADD_VOLUNTEER_START';
-export const ADD_VOLUNTEER_SUCCESS='ADD_VOLUNTEER_SUCCESS';
-export const ADD_VOLUNTEER_FAIL='ADD_VOLUNTEER_FAIL';
+export const ADD_VOLUNTEER_START = 'ADD_VOLUNTEER_START';
+export const ADD_VOLUNTEER_SUCCESS = 'ADD_VOLUNTEER_SUCCESS';
+export const ADD_VOLUNTEER_FAIL = 'ADD_VOLUNTEER_FAIL';
 
-// add a new volunteer
+export const addVolunteers = data => async dispatch => {
+  dispatch({
+    type: ADD_VOLUNTEER_START
+  });
 
-
-     
-    //     'fname': user.fname,
-    //     'lname': user.lname,
-    //     'email': user.email,
-    //     'phone': user.phone,
-    //     'city': user.city,
-    //     'state': user.state,
-    //     'country': user.country,
-    //     'comment': user.comment
-    // }
-
-    // const config = {
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // }
-
-    export const addVolunteers = (newVolunteer, newInterests ) =>  async dispatch => {
-        // console.log(obj.value);
-       dispatch({
-           type: ADD_VOLUNTEER_START
-       });
-        // let newVolunteer = 
-        axios
-          .post('https://miracle-messages-staging.herokuapp.com/api/form', newVolunteer, newInterests)
-          .then(res => {console.log("response", res)
-              return dispatch({
-                  type: ADD_VOLUNTEER_SUCCESS
-              });
-          })
-          .catch(err => {
-              return dispatch({ type: ADD_VOLUNTEER_FAIL, payload: err})
-          });
-
-    // return (dispatch) => {
-    //      dispatch({ 
-    //     type: ADDING_VOLUNTEER_START
-    //     })    
-
-    // request.then(res => {
-    //         dispatch({
-    //             type: ADDING_VOLUNTEER_SUCCESS, 
-    //             payload: res.data
-    //         });
-    //     }).catch(err => {
-    //         dispatch({ 
-    //             type: ADDING_VOLUNTEER_FAIL,
-    //             payload: err.data
-    //         });
-    //     });        
-    // }
-}
+  axios
+    .post('https://miracle-messages-staging.herokuapp.com/api/form', data)
+    .then(res => {
+      return dispatch({
+        type: ADD_VOLUNTEER_SUCCESS
+      });
+    })
+    .catch(err => {
+      return dispatch({ type: ADD_VOLUNTEER_FAIL, payload: err });
+    });
+};
--- miracle-messages/src/components/VolunteerForm.js
+++ miracle-messages/src/components/VolunteerForm.js
@@ -1,163 +1,229 @@
 import React from 'react';
-import '../CSS/form.css' 
-import { connect } from "react-redux"
+import '../CSS/form.css';
+import { connect } from 'react-redux';
 import { addVolunteers } from '../actions/FormActions';
 
-class VolunteerForm extends React.Component{
-    state = {
-        newVolunteer:{
-        fname: 'Jen',
-        lname: 'Harry',
-        email: 'lalal@tmal.com',
-        phone: '123456',
-        city: 'San Diego',
-        state: 'CAAAA',
-        country: 'usa',
-        comment: 'ejejejdjudjdncj'
+class VolunteerForm extends React.Component {
+  state = {
+    newVolunteer: {
+      fname: '',
+      lname: '',
+      email: '',
+      phone: '',
+      city: '',
+      state: '',
+      country: '',
+      comment: ''
     },
-        newInterests: {
-            volunteering: false,
-            donating:  false,
-            joinmm: false,
-            mediacoverage: false,
-            somethingelse: ''
-        }
-    };
-    
-  
-    handleOnsubmit = e => {    
-        console.log(`Form ${this.state.newInterests.volunteering} `)  
-        console.log(`Form 222 ${ this.state.newVolunteer.lname}`) 
-        e.preventDefault();
-        this.props.addVolunteers(this.state.newVolunteer,this.state.newInterests)
-        };
-        
-    handleToggle= e => {
-        // e.preventDefault();
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    } 
-    
-
-    handleOnChange = e => {
-        this.setState({
-           newVolunteer:{[e.target.name]: e.target.value }           
-        });console.log("here")
-    };
-
-    
-
-    render() {
-        return (
-            <form className="form"
-               onSubmit={this.handleOnsubmit}
-               loading={this.addVolunteers}
-            >
-            <h2>Get Involved</h2>
-            <input className="input"
-            type="text"
-            onChange={this.handleOnChange}
-            value={this.state.newVolunteer.fname}
-            name="fname"
-            placeholder="First Name"
-            required
-            /><br/>
-            <input className="input"
-            type="text"
-            onChange={this.handleOnChange}
-            value={this.state.newVolunteer.lname}
-            name="lname"
-            placeholder="Last Name"
-            required
-            /><br/>
-            <input className="input"
-            type="email"
-            onChange={this.handleOnChange}
-            value={this.state.newVolunteer.email}
-            name="email"
-            placeholder="E-mail"
-            required
-            /><br/>
-            <input className="input"
-            type="tel"
-            onChange={this.handleOnChange}
-            value={this.state.newVolunteer.phone}
-            name="phone"
-            placeholder="Phone Number"
-            /><br/>
-            <input className="input"
-            type="text"
-            onChange={this.handleOnChange}
-            value={this.state.newVolunteer.city}
-            name="city"
-            placeholder="City"
-            required
-            /><br/>
-            <input className="input"
-            type="text"
-            onChange={this.handleOnChange}
-            value={this.state.newVolunteer.state}
-            name="state"
-            placeholder="State"
-            required
-            /><br/>
-            <input className="input"
-            type="text"
-            onChange={this.handleOnChange} 
-            value={this.state.newVolunteer.country}
-            name="country"
-            placeholder="Country"
-            required
-            /><br/>
-                <h2>I am Interested In</h2>
-            <input onChange={this.handleToggle} className='chbox' type='checkbox'
-            name='volunteering'
-            value='volunteering'
-
-            />Volunteering<br/>
-            <input onChange={this.handleToggle} className='chbox' type='checkbox'
-            name='donating'
-            value='donating'
-            />Donating<br/>
-             <input onChange={this.handleToggle} className='chbox' type='checkbox'
-            name='joining'
-            value='joinmm'
-            />Join MM<br/>
-             <input className='chbox' type='checkbox'
-            name='donating'
-            value='donating'
-            />Donating<br/>
-             <input onChange={this.handleToggle} className='chbox' type='checkbox'
-            name='mediacoverage'
-            value='mediacoverage'
-            />Media Coverage<br/>
-             <input className='chbox' 
-             type='text'
-             onChange={this.handleOnChange}
-            name='somethingelse'
-            value={this.state.newInterests.somethingelse}
-            />Something Else<br/>
-            
-            
-            <textarea className="comment"
-            onChange={this.handleOnChange}
-            value={this.state.comment}
-            name="comment"
-            placeholder="Leave Your Comments"
-            /><br/>          
-            <button  className="submitb" type='submit'>Submit</button>
-            <button className="clearb"type='reset'>Clear</button>
-            </form>
-        );
-          
+    newInterests: {
+      volunteering: false,
+      donating: false,
+      joinmm: false,
+      mediacoverage: false,
+      somethingelse: ''
     }
+  };
 
+  handleOnsubmit = e => {
+    e.preventDefault();
+    this.props.addVolunteers(this.state);
+
+    this.setState({
+      newVolunteer: {
+        fname: '',
+        lname: '',
+        email: '',
+        phone: '',
+        city: '',
+        state: '',
+        country: '',
+        comment: ''
+      },
+      newInterests: {
+        volunteering: false,
+        donating: false,
+        joinmm: false,
+        mediacoverage: false,
+        somethingelse: ''
+      }
+    });
+  };
+
+  handleToggle = e => {
+    this.setState({
+      newInterests: {
+        ...this.state.newInterests,
+        [e.target.name]: e.target.checked
+      }
+    });
+  };
+
+  handleOnChange = e => {
+    this.setState({
+      newVolunteer: {
+        ...this.state.newVolunteer,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  handleOnChangeInterest = e => {
+    this.setState({
+      newInterests: {
+        ...this.state.newInterests,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <form
+        className="form"
+        onSubmit={this.handleOnsubmit}
+        loading={this.addVolunteers}
+      >
+        <h2>Get Involved</h2>
+        <input
+          className="input"
+          type="text"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.fname}
+          name="fname"
+          placeholder="First Name"
+          required
+        />
+        <br />
+        <input
+          className="input"
+          type="text"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.lname}
+          name="lname"
+          placeholder="Last Name"
+          required
+        />
+        <br />
+        <input
+          className="input"
+          type="email"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.email}
+          name="email"
+          placeholder="E-mail"
+          required
+        />
+        <br />
+        <input
+          className="input"
+          type="tel"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.phone}
+          name="phone"
+          placeholder="Phone Number"
+        />
+        <br />
+        <input
+          className="input"
+          type="text"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.city}
+          name="city"
+          placeholder="City"
+          required
+        />
+        <br />
+        <input
+          className="input"
+          type="text"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.state}
+          name="state"
+          placeholder="State"
+          required
+        />
+        <br />
+        <input
+          className="input"
+          type="text"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.country}
+          name="country"
+          placeholder="Country"
+          required
+        />
+        <br />
+        <h2>I am Interested In</h2>
+        <input
+          onChange={this.handleToggle}
+          className="chbox"
+          type="checkbox"
+          name="volunteering"
+          checked={this.state.newInterests.volunteering}
+        />
+        Volunteering
+        <br />
+        <input
+          onChange={this.handleToggle}
+          className="chbox"
+          type="checkbox"
+          name="donating"
+          checked={this.state.newInterests.donating}
+        />
+        Donating
+        <br />
+        <input
+          onChange={this.handleToggle}
+          className="chbox"
+          type="checkbox"
+          name="joinmm"
+          checked={this.state.newInterests.joinmm}
+        />
+        Join MM
+        <br />
+        <input
+          onChange={this.handleToggle}
+          className="chbox"
+          type="checkbox"
+          name="mediacoverage"
+          checked={this.state.newInterests.mediacoverage}
+        />
+        Media Coverage
+        <br />
+        <input
+          className="chbox"
+          type="text"
+          onChange={this.handleOnChangeInterest}
+          name="somethingelse"
+          value={this.state.newInterests.somethingelse}
+        />
+        Something Else
+        <br />
+        <textarea
+          className="comment"
+          onChange={this.handleOnChange}
+          value={this.state.newVolunteer.comment}
+          name="comment"
+          placeholder="Leave Your Comments"
+        />
+        <br />
+        <button className="submitb" type="submit">
+          Submit
+        </button>
+        <button className="clearb" type="reset">
+          Clear
+        </button>
+      </form>
+    );
+  }
 }
 
- const mapStateToProps = state => ({
-    //  volunteer: state.volunteerReducer.newVolunteer,
-    //  volunteers: state.volunteerReducer.addVolunteers
+const mapStateToProps = state => ({
+  //  volunteer: state.volunteerReducer.newVolunteer,
+  //  volunteers: state.volunteerReducer.addVolunteers
 });
 
-export default connect(mapStateToProps, {addVolunteers})(VolunteerForm);
+export default connect(
+  mapStateToProps,
+  { addVolunteers }
+)(VolunteerForm);
