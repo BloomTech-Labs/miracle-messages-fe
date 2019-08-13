15f1c4ec1f2154492f51565584a994be13215c53
HuMart <manya01@protonmail.com>
2019-08-13 13:35:16 -0400

    fixing the input to clear after submit

--- miracle-messages/src/Components/Login/LoginForm.js
+++ miracle-messages/src/Components/Login/LoginForm.js
@@ -17,7 +17,12 @@ class LoginForm extends React.Component {
     handleSubmit = e => {
         e.preventDefault();
         console.log(this.state);
-        this.props.fetchLogin(this.state)
+        this.props.fetchLogin(this.state);
+
+        this.setState({
+          username: "",
+          password: ""
+        });
     };
 
     handleOnChange = e => {
