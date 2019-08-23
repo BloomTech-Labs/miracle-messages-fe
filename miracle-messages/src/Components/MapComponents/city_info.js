b0e7b1b9d0da3c3cb3ecc880716f1af8584b752c
Navid Karami <navid.gsz@gmail.com>
2019-08-23 13:51:11 -0700

    moved asset forlder

--- miracle-messages/src/Components/MapComponents/city_info.js
+++ miracle-messages/src/Components/MapComponents/city_info.js
@@ -1,86 +1,85 @@
-
-import React, { PureComponent } from "react";
-import { connect } from "react-redux";
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 
 // Action imports
-import { slideToggleAction } from "../../Actions/SlideToggleAction";
-import { updatePopupAction } from "../../Actions/updatePopupAction";
+import { slideToggleAction } from '../../Actions/SlideToggleAction';
+import { updatePopupAction } from '../../Actions/updatePopupAction';
 
 // Icon imports
 // import facebook from "../../icons/facebook.png";
 // import google from "../../icons/google.png";
-import gmail from "../../Assests/icons/gmail.png";
+import gmail from '../../Assets/icons/gmail.png';
 
 // CSS imports
-import "../../CSS/city_info.css";
+import '../../CSS/city_info.css';
 
 class CityInfo extends PureComponent {
   render() {
     const { info } = this.props;
 
     return (
-      <div className="chapterInfo">
+      <div className='chapterInfo'>
         {/* Contains the chapter picture, name, and estblish date */}
-        <div className="chapterTitle">
+        <div className='chapterTitle'>
           <img
             src={info.chapter_img_url}
-            alt="Chapter"
-            height="200px"
-            width="100%"
+            alt='Chapter'
+            height='200px'
+            width='100%'
           />
-          <div className="title">Miracle Messages {info.city}</div>
-          <div className="date">{info.established_date}</div>
+          <div className='title'>Miracle Messages {info.city}</div>
+          <div className='date'>{info.established_date}</div>
         </div>
 
         {/* Contains the chapter statisics; members/reunions */}
-        <div className="chapterStats">
-          <p className="volunteers">
-            <span className="number">{info.numvolunteers}</span>
+        <div className='chapterStats'>
+          <p className='volunteers'>
+            <span className='number'>{info.numvolunteers}</span>
             Members
           </p>
-          <p className="reunions">
-            <span className="number">{info.numreunions}</span>
+          <p className='reunions'>
+            <span className='number'>{info.numreunions}</span>
             Reunions
           </p>
         </div>
 
         {/* Contains the chapters message */}
-        <div className="chapterDetails">{info.description}</div>
+        <div className='chapterDetails'>{info.description}</div>
 
         {/* Links out */}
-        <div className="buttons">
-          <button className="green">
+        <div className='buttons'>
+          <button className='green'>
             <a
-              href="https://miracle-messages-staging.netlify.com/form"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://miracle-messages-staging.netlify.com/form'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               JOIN CHAPTER
             </a>
           </button>
 
-          <button className="white">
-            <a href="" target="_blank" rel="noopener noreferrer">
+          <button className='white'>
+            <a href='' target='_blank' rel='noopener noreferrer'>
               SEE REUNION STORIES
             </a>
           </button>
         </div>
 
         {/* Contains the featured reunioun story */}
-        <div className="featuredReunion">
+        <div className='featuredReunion'>
           <h2>FEATURED REUNION STORY</h2>
           <img
             src={info.reunion_img_url}
-            alt="Chapter"
-            height="200px"
-            width="100%"
+            alt='Chapter'
+            height='200px'
+            width='100%'
           />
-          <div className="reunionStory">{info.story}</div>
+          <div className='reunionStory'>{info.story}</div>
           <a
-            href="https://miraclemessages.org/stories"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="stories"
+            href='https://miraclemessages.org/stories'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='stories'
           >
             See all reunion stories
           </a>
@@ -89,21 +88,20 @@ class CityInfo extends PureComponent {
         {/* Contains the contact info */}
         <div>
           <a
-            href="https://miraclemessages.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="contact"
+            href='https://miraclemessages.org'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='contact'
           >
             <span>
-              <img src={gmail} alt="gmail logo" className="gmailLogo" />
+              <img src={gmail} alt='gmail logo' className='gmailLogo' />
             </span>
-            <span className="email">{info.email}</span>
+            <span className='email'>{info.email}</span>
           </a>
         </div>
 
-
         {/* Contains the Sponser Images */}
-        <div className="sponsorImages">
+        <div className='sponsorImages'>
           <h2>SPONSORS</h2>
           {/* some images required */}
         </div>
