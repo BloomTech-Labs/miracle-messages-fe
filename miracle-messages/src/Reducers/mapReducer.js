f630e5a758ab0e87d4fe68534ffc65427c6ea553
Thierry Joux <tjoux@hotmail.com>
2019-08-23 15:50:02 -0700

    added Partners table to popupinfo in the map reducer

--- miracle-messages/src/Reducers/mapReducer.js
+++ miracle-messages/src/Reducers/mapReducer.js
@@ -36,7 +36,33 @@ const initialState = {
       "Lorem ipsum dolor sit amet, vim ea probo posse, te erant semper delectus mea. Ne dolorum officiis iracundia his. Expetenda definiebas id vel, munere constituto in sed. Ad est wisi forensibus, ad sed dolore propriae neglegentur, augue debitis quo ad. Agam bonorum his ad",
     story:
       "Lorem ipsum dolor sit amet, vim ea probo posse, te erant semper delectus mea. Ne dolorum officiis iracundia his. Expetenda definiebas id vel, munere constituto in sed. Ad est wisi forensibus, ad sed dolore propriae neglegentur, augue debitis quo ad. Agam bonorum his ad",
-    email: "john@miracle-messages.com"
+    email: "john@miracle-messages.com",
+    partners: [
+      {
+          "category": "partner",
+          "name": "SFPD",
+          "site_url": "https://www.sanfranciscopolice.org/",
+          "icon_url": "https://pbs.twimg.com/profile_images/880890495651545088/LcAV6z-c_400x400.jpg"
+      },
+      {
+          "category": "partner",
+          "name": "goodWill",
+          "site_url": "https://www.goodwill.org/",
+          "icon_url": "https://www.goodwill.org/wp-content/uploads/2019/06/cropped-Goodwill-Industries-International-Logo-1.jpg"
+      },
+      {
+          "category": "sponsor",
+          "name": "glide",
+          "site_url": "https://www.glide.org/",
+          "icon_url": "https://unm5i3x3smv2e4zlycj53ret-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/logo.svg"
+      },
+      {
+          "category": "sponsor",
+          "name": "National Charity League, LA",
+          "site_url": "https://www.nclla.org",
+          "icon_url": "https://www.nclla.org/wp-content/uploads/ncl-la-logo_2.svg"
+      }
+  ] 
     
   }, //null means no pop-ups are being rendered for any of the cities
   fetching: false,
