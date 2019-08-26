421eea2d3219be4792a861b06fa0a7f3597066b3
azrap <azraap@gmail.com>
2019-08-26 13:38:32 -0700

    added the correct links/language  for GET INVOLVED and DONATE buttons. Changed the hard coded default SF initial state to have correct data

--- miracle-messages/src/Components/MapComponents/city_info.js
+++ miracle-messages/src/Components/MapComponents/city_info.js
@@ -24,68 +24,72 @@ class CityInfo extends PureComponent {
     console.log('PARTNERS: ', partners );
 
     return (
-      <div className='chapterInfo'>
+      <div className="chapterInfo">
         {/* Contains the chapter picture, name, and estblish date */}
-        <div className='chapterTitle'>
+        <div className="chapterTitle">
           <img
             src={info.chapter_img_url}
-            alt='Chapter'
-            height='200px'
-            width='100%'
+            alt="Chapter"
+            height="200px"
+            width="100%"
           />
-          <div className='title'>Miracle Messages {info.city}</div>
-          <div className='date'>{info.established_date}</div>
+          <div className="title">Miracle Messages {info.city}</div>
+          <div className="date">{info.established_date}</div>
         </div>
 
         {/* Contains the chapter statisics; members/reunions */}
-        <div className='chapterStats'>
-          <p className='volunteers'>
-            <span className='number'>{info.numvolunteers}</span>
+        <div className="chapterStats">
+          <p className="volunteers">
+            <span className="number">{info.numvolunteers}</span>
             Members
           </p>
-          <p className='reunions'>
-            <span className='number'>{info.numreunions}</span>
+          <p className="reunions">
+            <span className="number">{info.numreunions}</span>
             Reunions
           </p>
         </div>
 
         {/* Contains the chapters message */}
-        <div className='chapterDetails'>{info.description}</div>
+        <div className="chapterDetails">{info.description}</div>
 
         {/* Links out */}
-        <div className='buttons'>
-          <button className='green'>
+        <div className="buttons">
+          <button className="green">
             <a
-              href='https://miracle-messages-staging.netlify.com/form'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://miraclemessages.org/getinvolved"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              JOIN CHAPTER
+              GET INVOLVED
             </a>
           </button>
 
-          <button className='white'>
-            <a href='' target='_blank' rel='noopener noreferrer'>
-              SEE REUNION STORIES
+          <button className="white">
+            <a
+              href="https://www.classy.org/give/231839/#!/donation/checkout"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              DONATE
             </a>
           </button>
         </div>
 
         {/* Contains the featured reunioun story */}
-        <div className='featuredReunion'>
+        <div className="featuredReunion">
           <h2>FEATURED REUNION STORY</h2>
           <img
             src={info.reunion_img_url}
-            alt='Chapter'
-            height='200px'
-            width='100%'
+            alt="Chapter"
+            height="200px"
+            width="100%"
           />
-          <div className='reunionStory'>{info.story}</div>
+          <div className="reunionStory">{info.story}</div>
           <a
-            href='https://miraclemessages.org/stories'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='stories'
+            href="https://miraclemessages.org/stories"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="stories"
           >
             See all reunion stories
           </a>
@@ -94,66 +98,63 @@ class CityInfo extends PureComponent {
         {/* Contains the contact info */}
         <div>
           <a
-            href='https://miraclemessages.org'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='contact'
+            href="https://miraclemessages.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact"
           >
             <span>
-              <img src={gmail} alt='gmail logo' className='gmailLogo' />
+              <img src={gmail} alt="gmail logo" className="gmailLogo" />
             </span>
-            <span className='email'>{info.email}</span>
+            <span className="email">{info.email}</span>
           </a>
         </div>
 
         {/* Contains the Sponsor Images */}
-            {
-              sponsors.length > 0 && (
-                <div className='sponsors'>
-                  <h2>SPONSORS</h2>
-                  <div className='icons-container'>
-                  {
-                    sponsors.map((sponsor, index) => 
-                      <a href={sponsor.site_url} target="_blank" key={`sponsor-${index}`}>
-                        <div className='icon-container'>
-                          <img
-                            src={sponsor.icon_url}
-                            alt="Icon"
-                            height="75px"
-                            width='57.71px' 
-                          />
-                        </div>
-                      </a>
-                    )
-                  }
+        {sponsors.length > 0 && (
+          <div className="sponsors">
+            <h2>SPONSORS</h2>
+            <div className="icons-container">
+              {sponsors.map((sponsor, index) => (
+                <a
+                  href={sponsor.site_url}
+                  target="_blank"
+                  key={`sponsor-${index}`}
+                >
+                  <div className="icon-container">
+                    <img
+                      src={sponsor.icon_url}
+                      alt="Icon"
+                      height="75px"
+                      width="57.71px"
+                    />
                   </div>
-                </div>
-              )
-            }
+                </a>
+              ))}
+            </div>
+          </div>
+        )}
 
         {/* Contains the Partner Images */}
-          {
-            partners.length > 0 && (
-              <div className='partners'>
-                <h2>PARTNERS</h2>
+        {partners.length > 0 && (
+          <div className="partners">
+            <h2>PARTNERS</h2>
 
-                <div className='icons-container'>
-                  {
-                    partners.map((partner, index) => 
-                    <a href={partner.site_url} target="_blank" key={`partner-${index}`}>
-                      <div className='icon-container'>
-                        <img
-                          src={partner.icon_url}
-                          alt="Icon"
-                        />
-                      </div>
-                    </a>
-                    )
-                  }
-                </div>
-              </div>
-            )
-          }
+            <div className="icons-container">
+              {partners.map((partner, index) => (
+                <a
+                  href={partner.site_url}
+                  target="_blank"
+                  key={`partner-${index}`}
+                >
+                  <div className="icon-container">
+                    <img src={partner.icon_url} alt="Icon" />
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
