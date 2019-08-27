f45fbf0e1e66fbcd3754a16ca4397cfca3a4bf7a
Greyflanel <richardlovelace811@gmail.com>
2019-08-27 12:42:38 -0500

    added cypress testing for the addChapter form

---
+++ miracle-messages/cypress/integration/Admin_spec.js
@@ -0,0 +1,59 @@
+/// enables intellisense for cypress
+/// <reference types="Cypress" />
+
+/* eslint-disable no-undef */
+describe("Admin page", function() {
+  it("should successfully load the admin page", function() {
+    cy.visit("/admin/chapters");
+  });
+
+  it("should get all the chapter elements", function() {
+    cy.get(".card").then($el => {
+      $el.get();
+    });
+  });
+
+  it("should have state on load", () => {
+    cy.window()
+      .its("store")
+      .invoke("getState")
+      .should("exist");
+  });
+
+  it("should get the Edit button for first Chapter", function() {
+    cy.get(".btn")
+      .eq(0)
+      .should("have.length", 1);
+  });
+
+  it("should get the Edit button for first Chapter", function() {
+    cy.get(".btn-secondary").eq(0).contains('Edit')
+      
+  });
+
+  it("should click add Chapter button", function() {
+    cy.get(".addBtn").click()
+    
+  });
+
+  it("should fill all input fields to add a Chapter", function() {
+    cy.get("input").eq(0).type("This is how we do it!", { delay: 100 })
+    cy.get("input").eq(1).type("1981-02-20", { delay: 100 })
+    cy.get(".form-control").eq(2).type("One day you'll wake up, and this will all be just a dream.", { delay: 50 })
+    cy.get("input").eq(3).type("Norfolk", { delay: 100 })
+    cy.get("input").eq(4).type("VA", { delay: 100 })
+    cy.get("input").eq(5).type("36.8508", { delay: 100 })
+    cy.get("input").eq(6).type("76.2859", { delay: 100 })
+    cy.get("input").eq(7).type("greyflanel@.com", { delay: 100 })
+    cy.get("input").eq(8).type("26", { delay: 100 })
+    cy.get("input").eq(9).type("72", { delay: 100 })
+    cy.get("input").eq(10).type("9", { delay: 100 })
+    cy.get("input").eq(11).type("86", { delay: 100 })
+    cy.get(".form-control").eq(12).type("Once upon a time, not long ago. Where people wore pajamas and lived life slow....", { delay: 50 })
+  });
+
+  it("should click the button to cancel add Chapter", function() {
+    cy.get(".btn-secondary").eq(23).click()
+  })
+
+});
