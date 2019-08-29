502720bde9d1f6483bd83512609fc033727e1596
Greyflanel <richardlovelace811@gmail.com>
2019-08-29 11:41:04 -0500

    fixed bugs that appeared after merging with staging-server

--- miracle-messages/cypress/integration/Admin_Login_spec.js
+++ miracle-messages/cypress/integration/Admin_Login_spec.js
@@ -10,12 +10,19 @@ describe("Login page", function() {
     });
 
     it("should fill input fields", function() {
-        cy.get(".input").first().type('username', { delay: 100 });
-        cy.get(".input").eq(1).type('password', { delay: 100 })
+        cy.get("input").first().type('admin', { delay: 100 });
+        cy.get("input").eq(1).type('superuser01', { delay: 100 })
         
       });
 
       it("should click to submit", function() {
-        cy.get(".submitb").click();
+        cy.get(".btn-primary").click();
       });
+
+      it("should click the sidebar link for Chapters", function() {
+    cy.get(".sidebar-link")
+      .eq(0)
+      .click()
+      .wait(2000);
+  });
 })
