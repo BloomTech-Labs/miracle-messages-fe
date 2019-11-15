39cc5264000fc51f2eb375bb390b03229ba07efe
Adrian-Garcia <agarc5@hotmail.com>
2019-11-15 13:11:15 -0600

    login created, need to figure out where the login will be routed

--- miracle-messages/src/App.js
+++ miracle-messages/src/App.js
@@ -1,27 +1,26 @@
-
-import React, { Component } from 'react';
-import Map from './Components/MapComponents/map';
-import './CSS/App.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import indexRoutes from './Components/dashboard/routes/index.js';
+import React, { Component } from "react";
+import Map from "./Components/MapComponents/map";
+import "./CSS/App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import indexRoutes from "./Components/dashboard/routes/index.js";
 import VolunteerForm from "./Components/Forms/VolunteerForm";
-import LoginFrom from './Components/Login/LoginForm.js';
-import { PrivateRoute } from './Components/PrivateRoute';
-import './CSS/style.css';
-
+import LoginForm from "./Components/Login/LoginForm.js";
+import { PrivateRoute } from "./Components/PrivateRoute";
+import "./CSS/style.css";
+import LoginPage from "./Components/Login/UserLogin";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
-
-          <Route exact path='/' component={Map} />
+          <Route exact path="/" component={Map} />
 
           <Route exact path="/form" component={VolunteerForm} />
-          
-          <Route exact path='/login' component={LoginFrom} />
-          
+
+          <Route exact path="/login" component={LoginForm} />
+
+          <Route exact path="/user/login" component={LoginPage} />
 
           {indexRoutes.map((prop, index) => {
             return (
@@ -30,15 +29,14 @@ class App extends Component {
                 key={index}
                 component={prop.component}
               />
-            )
+            );
           })}
 
-          <Redirect from='*' to='/' />
-
+          <Redirect from="*" to="/" />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
