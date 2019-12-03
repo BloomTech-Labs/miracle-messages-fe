11bb66caf06a9d9a4c6a88a88a26f0b00d28997d
noahfranco <noahfra@gmail.com>
2019-12-03 18:20:07 -0500

    built out Actions

---
+++ miracle-messages/src/Actions/SearchBarAction.js
@@ -0,0 +1,22 @@
+import { axiosWithAuth } from "./AxiosWithAuth";
+
+export const GET_CHAPTERS_SUCCESS = "GET_CHAPTERS_SUCCESS"; 
+export const GET_CHAPTERS_FAIL = "GET_CHAPTERS_FAIL"; 
+
+export const getChapter = chapter => async dispatch => {
+     dispatch({
+        type: GET_CHAPTERS_SUCCESS
+    }); 
+
+    axiosWithAuth()
+    .get("localhost:5000/api/chapter", chapter)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(error => {
+        console.log(error)
+        dispatch: ({
+            type: GET_CHAPTERS_FAIL
+        });
+    });
+}
