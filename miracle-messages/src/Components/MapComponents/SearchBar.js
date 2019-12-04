d3a724782198935e2e288a6961263eb9db36006c
noahfranco <noahfra@gmail.com>
2019-12-04 13:23:11 -0500

    started building search component

---
+++ miracle-messages/src/Components/MapComponents/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react"; 
+import getChapter from "../../Actions/SearchBarAction.js"; 
+
+const SearchBar = () => {
+    
+
+    useEffect(() => {
+        getChapter
+    }, [])
+
+    const handleChange = () => {
+        
+    }
+
+    const handleSubmit = () => {
+
+    }
+
+    return(
+        <>
+        <form> 
+            <input 
+
+            />
+        </form>
+        </>
+    );
+}; 
