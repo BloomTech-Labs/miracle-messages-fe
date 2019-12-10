0529f0d2775224a76aceb66bf287842c35e7c038
noahfranco <noahfra@gmail.com>
2019-12-10 15:26:10 -0500

    centered card

--- miracle-messages/src/Components/MapComponents/SearchBarCard.js
+++ miracle-messages/src/Components/MapComponents/SearchBarCard.js
@@ -5,7 +5,9 @@ const SearchBarCard = props => {
     console.log(props)
     return(
         <div className="card">
+        <div classNam="img-center"> 
         <img className="width" src={props.chapter.chapter_img_url} />
+        </div>
         
         <h3 className="center"> city: </h3> 
         <p className="center"> {props.chapter.city} </p> 
