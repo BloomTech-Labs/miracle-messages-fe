8a00443f45af72a590e8516a4db61fea338c20bd
MadisonViaLux <madison.s.mckown@gmail.com>
2019-12-11 14:27:07 -0700

    testing bug

--- miracle-messages/src/Components/MapComponents/Sidebar.js
+++ miracle-messages/src/Components/MapComponents/Sidebar.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
-import './Sidebar.scss';
+import "./Sidebar.scss";
 export default props => {
   return (
     // Pass on our props
     <Menu {...props}>
-        <a className='menu-item' href='https://miraclemessages.org/'>HOME</a>
-          <a className='menu-item' href='https://miraclemessages.org/who'>ABOUT</a>
-          <a className='menu-item' href='https://miraclemessages.org/partner'>REUNION SERVICE</a>
-          <a className='menu-item' href='http://localhost:3000/form'>GET INVOLVED</a>
-          <a className='menu-item' href='https://www.classy.org/give/231839/#!/donation/checkout'>
-            DONATE
-          </a>
+      <a className="menu-item" href="https://miraclemessages.org/">
+        HOME
+      </a>
+      <a className="menu-item" href="https://miraclemessages.org/who">
+        ABOUT
+      </a>
+      <a className="menu-item" href="https://miraclemessages.org/partner">
+        REUNION SERVICE
+      </a>
+      <a className="menu-item" href="http://localhost:3000/form">
+        GET INVOLVED
+      </a>
+      <a
+        className="menu-item"
+        href="https://www.classy.org/give/231839/#!/donation/checkout"
+      >
+        DONATE
+      </a>
     </Menu>
   );
 };
