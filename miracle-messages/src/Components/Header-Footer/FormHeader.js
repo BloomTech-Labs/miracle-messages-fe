ab5a596d10b847d8f4be26a2ebbee059ea9cfdb2
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-12-21 17:23:17 -0700

    updating the Header to have the navbar drop down

--- miracle-messages/src/Components/Header-Footer/FormHeader.js
+++ miracle-messages/src/Components/Header-Footer/FormHeader.js
@@ -1,6 +1,6 @@
 import React from "react"
 import logo from "../../Assets/Imgs/MM_Logo.png"
-
+import "../MapComponents/Navbar.scss"
 /*******************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************In order for this to work, you need to render inside of the <div className="container"> div of the component you want it to render in.
  ******************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
  */
@@ -27,10 +27,25 @@ const FormHeader = () => {
             >
               <div>REUNION SERVICE</div>
             </a>
-            <a href="localhost:3000/form">
-              <div>GET INVOLVED</div>
-              {/* this anchor tag should have a drop down to the register page, login form, forgot password page */}
-            </a>
+            <div className="dropdown">
+              <a className="dropbtn">GET INVOLVED</a>
+              <div className="dropdown-content">
+                <a href="https://miracle-message.netlify.com/form">
+                  Volunteer Registration
+                </a>
+                <a href="https://miracle-message.netlify.com/user/login">
+                  Volunteer Login
+                </a>
+                <a href="https://miracle-message.netlify.com/user/newchapter">
+                  Create a Chapter
+                </a>
+                <a href="https://miracle-message.netlify.com/admin/login">
+                  Admin Login
+                </a>
+                <a className="not-last-child">test</a>
+              </div>
+            </div>
+
             <a
               href="https://www.classy.org/give/231839/#!/donation/checkout"
               target="_blank"
