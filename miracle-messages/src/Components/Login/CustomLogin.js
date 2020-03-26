72bded2f60a026aadec40fd69ca783011b5a4f29
unknown <viola4lfe@gmail.com>
2020-03-26 16:28:07 -0400

    New branch, working Okta-login

--- miracle-messages/src/Components/Login/CustomLogin.js
+++ miracle-messages/src/Components/Login/CustomLogin.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import CustomLoginForm from './CustomLoginForm';
-import { useOktaAuth } from '@okta/okta-react';
+import React from "react";
+import { Redirect } from "react-router-dom";
+import CustomLoginForm from "./CustomLoginForm";
+import { useOktaAuth } from "@okta/okta-react";
 
-const CustomLogin = ({ baseUrl }) => { 
+const CustomLogin = ({ baseUrl }) => {
   const { authState } = useOktaAuth();
 
-  if (authState.isPending) { 
+  if (authState.isPending) {
     return <div>Loading...</div>;
   }
-  return authState.isAuthenticated ?
-    <Redirect to={{ pathname: '/admin' }}/> :
-    <CustomLoginForm baseUrl={baseUrl} />;
+  return authState.isAuthenticated ? (
+    <Redirect to={{ pathname: "/admin" }} />
+  ) : (
+    <CustomLoginForm baseUrl={baseUrl} />
+  );
 };
 
-export default CustomLogin
+export default CustomLogin;
