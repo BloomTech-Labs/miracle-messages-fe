207d1be56f2a4e4f4c4a9857b7f22eb0bcc54c93
Daniel Mattox <dmattox10@gmail.com>
2020-03-26 15:40:33 -0400

    Changed signin form to widget

--- miracle-messages/src/config/config.js
+++ miracle-messages/src/config/config.js
@@ -1,14 +1,14 @@
-const CLIENT_ID = process.env.CLIENT_ID || '0oa4stu89kEpHK1sZ4x6';
-const ISSUER = process.env.ISSUER || 'https://donyawebgroup-us.okta.com/oauth2/default';
+const CLIENT_ID = process.env.CLIENT_ID || '0oa4sulf3qW8OTVdF4x6';
+const ISSUER = process.env.ISSUER || 'https://dev-662389.okta.com/oauth2/default';
 //might not need profile 
 export default {
     oidc: {
       clientId: CLIENT_ID,
       issuer: ISSUER,
-      // redirectUri: window.location.origin + '/implicit/callback',
-      redirectUri: 'http://localhost:3001/implicit/callback',
+      redirectUri: window.location.origin + '/implicit/callback',
+      //redirectUri: 'http://localhost:3001/implicit/callback',
       scopes: ['openid', 'profile', 'email'],
-      // onAuthRequired={onAuthRequired},
+      //onAuthRequired={onAuthRequired},
       pkce: true,
     },
     /*
