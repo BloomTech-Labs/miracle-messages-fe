56b9483b0f6e299f4c321ea855e2b9cf52b5012e
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-03-30 20:07:17 -0400

    Dynamically create callback URL

--- miracle-messages/src/config/config.js
+++ miracle-messages/src/config/config.js
@@ -1,7 +1,14 @@
 const CLIENT_ID = process.env.CLIENT_ID || '0oa4z3i22O2AsShIr4x6';
 const ISSUER = process.env.ISSUER || 'https://dev-636641.okta.com/oauth2/default';
-const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI || 'https://friday-okta-implementation.d1v4uoi0wi2hmy.amplifyapp.com/implicit/callback';
-const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+
+// The callback path always goes to the same route
+const CALLBACK_PATH = '/implicit/callback';
+
+// The callback may be on HTTP or HTTPS; default to HTTPS
+const CALLBACK_SSL = process.env.REACT_APP_CALLBACK_SSL || true
+
+// The callback should always come back to this host; will be http or https depending on the setting
+const REDIRECT_URI = `${CALLBACK_SSL == true ? "https" : "http"}://${window.location.host}${CALLBACK_PATH}`;
 
 export default {
   oidc: {
@@ -10,7 +17,9 @@ export default {
     redirectUri: REDIRECT_URI,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
-    disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+
+    // If the callback is _not_ over SSL, disable the HTTPS check in the client
+    disableHttpsCheck: !CALLBACK_SSL,
   },
   resourceServer: {
     messagesUrl: 'http://localhost:8000/api/messages',
