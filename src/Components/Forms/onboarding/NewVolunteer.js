bb5c3a035474996795fdbf802dd157430487105a
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-04-14 12:33:33 -0700

    i think its done?

--- src/Components/Forms/onboarding/NewVolunteer.js
+++ src/Components/Forms/onboarding/NewVolunteer.js
@@ -7,7 +7,8 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import stepperImage1 from '../../../Assets/Imgs/stepper-image-1.jpeg';
 import mmLogo from '../../../Assets/Imgs/MM_Logo.png';
-
+import mmLogoBlack from '../../../Assets/Imgs/MM_Logo_black.png';
+import './newVolunteer.css';
 
 
 //material Ui
@@ -28,7 +29,9 @@ imageSide: {
     display: 'flex',
     flexFlow: 'row wrap',
     justifyContent: 'center',
-    
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
   },
   imgContainer: {
     width: '100%',
@@ -45,24 +48,57 @@ imageSide: {
     color: '#fff',
     width: '100%',
     textAlign: 'center',
-    padding: '0 10%',
+    padding: '0 25%',
+    marginTop: '15px',
   },
   stepper: {
     width: '100%',
+    margin: '0 20%',
+    background: 'transparent',
+    color: 'green',
   },
   stepperSide: {
     width: '60%',
     display: 'flex',
     flexFlow: 'row wrap',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    padding: '11%',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
+   
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    '& button': {
+      width:  '100px',
+    },
   },
   button: {
-    marginRight: theme.spacing(1),
+    margin: '0 5px',
+    backgroundColor: 'green',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#008000a8'
+    },
+  },
+  leftButton: {
+    margin: '0 5px',
+    backgroundColor: 'white',
+    border: 'solid green 1px',
+    color: 'green',
+    '&:hover': {
+      backgroundColor: '#008000a8',
+      color: 'white',
+      border: 'solid #008000a8 1px',
+    },
   },
   instructions: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1),
-  },
+   
   link: {
       color: 'black',
       textDecoration: 'none',
@@ -70,7 +106,22 @@ imageSide: {
         color: 'green',
       }
   }
-
+  },
+    stepInner: {
+      '& svg': {
+        color: '#008000a8',
+      }
+    },
+  mobileLogo: {
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+    },
+  },
+  formContainer: {
+    textAlign: 'center',
+  } 
+  
 }));
 
 function getSteps() {
@@ -78,33 +129,42 @@ function getSteps() {
 }
 
 function getStepContent(step) {
+
   switch (step) {
     case 0:
       return (
       <>
-          <h1>You have selected to connect with this City chapter.</h1> 
-          <h3>If that is the place you would like to vounteer click next.</h3>
+          <h3>You have selected to connect with this City chapter.</h3> 
+          <p>If that is the place you would like to vounteer click next.</p>
       </>
       );
     case 1:
       return ( 
       <>
       <div > 
-      <h1 >You've taken the first step!</h1>
-      <h3 className='.potterStyle'>We just need a little more information to get started</h3>
+      <h3 >You've taken the first step!</h3>
+      <p>We just need a little more information to get started</p>
       <br/>
-      <TextField style={{ color: 'red'}} id="name" label="Name" variant="outlined" />
-      <br/>
-      <TextField id="outlined-basic" type='email' label="Email" variant="outlined" />
-      </ div>
+      <div className='textFieldContainer'>
+        <TextField className='inputField' id="firstname" label="First Name" variant="outlined" />
+        <br/>
+        <TextField className='inputField' id="outlined-basic" type='lastname' label="Last Name" variant="outlined" />
+        <br/>
+        <TextField className='inputField' id="outlined-basic" type='city' label="City" variant="outlined" />
+        <br/>
+        <TextField className='inputField' id="outlined-basic" type='country' label="Country" variant="outlined" />
+        <br/>
+        <TextField className='inputField' id="outlined-basic" type='email' label="Email" variant="outlined" />
+        </ div>
+      </div>
        </>
        );
     case 2:
       return (
         <>
         <div >
-        <h1>Thank you for your commitment to your community!</h1>
-        <h3>We will be contacting you shortly</h3>
+        <h3>Thank you for your commitment to your community!</h3>
+        <p>We will be contacting you shortly</p>
         </div>
          </>
       );
@@ -112,8 +172,8 @@ function getStepContent(step) {
       return '' (
           <>
           <div >  
-          <h1>Thank you for your commitment to your community!</h1>
-          <h3> We will be contacting you shortly.</h3>
+          <h3>Thank you for your commitment to your community!</h3>
+          <p> We will be contacting you shortly.</p>
           </div>
           </>
       );
@@ -121,11 +181,11 @@ function getStepContent(step) {
 }
 
 const NewVolunteer = () => {
-    const classes = useStyles();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [skipped, setSkipped] = React.useState(new Set());
-    const steps = getSteps();
-    
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+  const steps = getSteps();
+  
 
   const isStepOptional = (step) => {
     return step === 1;
@@ -173,30 +233,44 @@ const NewVolunteer = () => {
     <div className={classes.root}>
         <div className={classes.imageSide}>
             <div className={classes.imgContainer}>
-                <img className={classes.imgStyling} src={mmLogo} alt="Miracle Messages Logo"/>
+                {/* <div className={classes.imgOverlay}> */}
+                  <img className={classes.imgStyling} src={mmLogo} alt="Miracle Messages Logo"/>
+                {/* </div> */}
                 <h3 className={classes.h3Styling}>Help us reconnect people with their loved ones.</h3>
             </div>
         </div>
         <div className={classes.stepperSide} >
-      <Stepper className={classes.stepper} activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = <Typography variant="caption"></Typography>;
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
+          <div className={classes.mobileLogo}>
+            <img src={mmLogoBlack} />
+          </div>
+
+        <Stepper className={classes.stepper} activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            if (isStepOptional(index)) {
+              labelProps.optional = <Typography variant="caption"></Typography>;
+            }
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step 
+              key={label} 
+              {...stepProps}
+              >
+                <StepLabel 
+                className= {classes.stepInner} 
+                {...labelProps}
+                >
+                  {label}
+                </StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
         {activeStep === steps.length ? (
-          <div>
+          <div  className={classes.buttonContainer}>
             <Typography className={classes.instructions}>
               All steps completed - you&apos;re finished
             </Typography>
@@ -205,28 +279,17 @@ const NewVolunteer = () => {
             </Button>
           </div>
         ) : (
-          <div>
+          <div className={classes.formContainer} >
             <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-            <div>
+            <div className={classes.buttonContainer}>
               {/* <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
                   BACK
               </Button> */}
                 {activeStep === steps.length - 1 ? <a href="google.com/" className={classes.link}>
                   FAQ
-              </a> : <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+              </a> : <Button disabled={activeStep === 0} onClick={handleBack} className={classes.leftButton}>
                   BACK
               </Button>}
-              {isStepOptional(activeStep) && (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleSkip}
-                  className={classes.button}
-                >
-                  Skip
-                </Button>
-              )}
-
               <Button
                 variant="contained"
                 color="primary"
@@ -238,7 +301,8 @@ const NewVolunteer = () => {
             </div>
           </div>
         )}
-      </div>
+
+        </div>
     </div>
   );
 }
