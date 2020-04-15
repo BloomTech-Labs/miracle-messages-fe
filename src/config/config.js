a2ea0c273a08314f20fce18432861cadf3572f7e
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-04-15 10:00:12 -0700

    added a if statetment for development or prodction using the correct REDIRECT_URI url

--- src/config/config.js
+++ src/config/config.js
@@ -8,8 +8,13 @@ const CALLBACK_PATH = '/implicit/callback';
 const CALLBACK_SSL = process.env.REACT_APP_CALLBACK_SSL || true
 
 // The callback should always come back to this host; will be http or https depending on the setting
-const REDIRECT_URI = `${CALLBACK_SSL == true ? "https" : "http"}://${window.location.host}${CALLBACK_PATH}`;
-// const REDIRECT_URI = `http://localhost:3000/implicit/callback`;
+
+let REDIRECT_URI;
+if(process.env.NODE_ENV === 'development') {
+   REDIRECT_URI = `http://localhost:3000/implicit/callback`;
+}  else {
+   REDIRECT_URI = `${CALLBACK_SSL == true ? "https" : "http"}://${window.location.host}${CALLBACK_PATH}`;
+}
 
 export default {
   oidc: {
