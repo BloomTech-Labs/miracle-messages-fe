7f01a499fe3cf2eda39acc165edffdae3f00cbd6
Sam Ardis <ardis.productions@gmail.com>
2020-05-14 17:22:25 -0400

    Added some style changes to the navigation and searchbar, changed route/links to go to one login page

--- src/Components/MapComponents/Navbar.js
+++ src/Components/MapComponents/Navbar.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-import {
-  Nav, NavbarBrand, Collapse, DropdownItem, UncontrolledDropdown, DropdownToggle, DropdownMenu
-} from 'reactstrap';
 import { Link } from "react-router-dom";
 import logo from "../../Assets/Imgs/MM_Logo.png";
 import { connect } from "react-redux";
@@ -14,6 +11,7 @@ import axios from "axios";
 import { useOktaAuth } from '@okta/okta-react';
 
 import profilephoto from '../../Assets/Imgs/USER-PROF.png';
+import { Media } from "reactstrap";
 
 const Navbar = (props) => {
   const [search, updateSearch] = useState("");
@@ -45,7 +43,13 @@ const Navbar = (props) => {
   return (
     <div className="navbar-map">
       <Link to="/">
-        <img src={logo} alt="logo" />
+        <img style={{
+          paddingTop: '20px',
+          paddingBottom: '10px',
+          paddingLeft: '20px',
+          width: '160px'
+        }}
+          src={logo} alt="logo" />
       </Link>
       <nav>
         <div className="search-box">
@@ -70,20 +74,25 @@ const Navbar = (props) => {
         <Link to="/">Dashboard</Link>
 
         {/* create registration/login navigation */}
-        <Link className="dropbtn" to="/login">
+        <Link to="/login">
           GET INVOLVED
         </Link>
 
         <Link to='/'>Map</Link>
 
-        <a target='_blank' href="https://www.classy.org/give/231839/#!/donation/checkout">
+        <a target='_blank' href="https://www.classy.org/give/231839/#!/donation/checkout" 
+        style={{
+          border: '1px solid white',
+          padding: '10px 10px',
+          backgroundColor: 'white',
+          borderRadius: '10px',
+          color: '#212121'
+        }}>
           DONATE
         </a>
-        <Link to='/' 
+        <Link to='/' className='navProPic'
                   style={{
-                    width: '9%',
-                    padding: '10px 10px',
-                    background: '#212121'
+                    background: '#212121',
                   }}>
                   <img 
                     src={profilephoto}
