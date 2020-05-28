1d7616b8140d33b92bbb67b3ced4d2d2d8c7ebd7
Alexander Karren <alex@karren.com>
2020-05-28 13:26:10 -0600

    sidebar tabs stay highlighted when on respective page

--- src/Components/dashboard/sidebar/sidebar.js
+++ src/Components/dashboard/sidebar/sidebar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import './Sidebar.scss'
 
 import { useUserGroups } from '../../../utils/customHooks/useUserGroups';
@@ -7,15 +7,18 @@ import { useUserGroups } from '../../../utils/customHooks/useUserGroups';
 
 const Sidebar = ({ setSideBarOpen, sideBarOpen }) => {
   const { admin, chapterLeaders, volunteer } = useUserGroups();
-  const { push, location } = useHistory();
+  const { push } = useHistory();
+  const { pathname } = useLocation();
+
+  useEffect(() => console.log("poo"), [sideBarOpen])
 
     return (
         <div className={sideBarOpen ? "sidebar" : "sidebar hidden"}>
-          <div onClick={() => push("/admin/chapters")}><i className="fas fa-building"></i><div>Chapters</div></div>
+          <div className={pathname.includes("admin/chapters") ? "active-tab" : ""} onClick={() => push("/admin/chapters")}><i className="fas fa-building"></i><div>Chapters</div></div>
           {/* only for admins */}
-          <div onClick={() => push("/admin/pending")}><i className="fas fa-wrench"></i><div>Pending</div></div>
-          <div onClick={() => push("/admin/Sponsors")}><i className="fas fa-hands-helping"></i><div>Sponsors</div></div>
-          <div onClick={() => push("/admin/volunteers")}><i className="fas fa-users"></i><div>Volunteers</div></div>
+          <div className={pathname.includes("admin/pending") ? "active-tab" : ""} onClick={() => push("/admin/pending")}><i className="fas fa-wrench"></i><div>Pending</div></div>
+          <div className={pathname.includes("admin/Sponsors") ? "active-tab" : ""} onClick={() => push("/admin/Sponsors")}><i className="fas fa-hands-helping"></i><div>Sponsors</div></div>
+          <div className={pathname.includes("admin/volunteers") ? "active-tab" : ""} onClick={() => push("/admin/volunteers")}><i className="fas fa-users"></i><div>Volunteers</div></div>
           <div onClick={() => setSideBarOpen(false)}><i className="fas fa-times"></i><div>Close</div></div>
         </div>
     )
