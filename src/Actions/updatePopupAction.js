9fe9f6fc3f883dc3e1a48fa1cb8457941be3e352
Shun Chiang <shunchiang@Shuns-MacBook-Pro.local>
2020-06-17 21:07:06 -0500

    added reunion popups with videos

--- src/Actions/updatePopupAction.js
+++ src/Actions/updatePopupAction.js
@@ -1,14 +1,20 @@
 export const UPDATE_POPUP = "UPDATE_POPUP";
+export const UPDATE_REUNION_POPUP = "UPDATE_REUNION_POPUP";
 
 //this action enables us to display or hide a pop-up for the city
 
-export const updatePopupAction = popupinfo => dispatch => {
+export const updatePopupAction = (popupinfo) => (dispatch) => {
   dispatch({
     type: UPDATE_POPUP,
-    payload: popupinfo
+    payload: popupinfo,
   });
 };
 
-
+export const updateReunuinAction = (popupinfo) => (dispatch) => {
+  dispatch({
+    type: UPDATE_REUNION_POPUP,
+    payload: popupinfo,
+  });
+};
 
 export default updatePopupAction;
